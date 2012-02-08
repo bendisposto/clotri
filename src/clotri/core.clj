36ef4bcc33ceb7e068d20710bbf9d28169762753
Jens Bendisposto <jens@bendisposto.de>
2012-02-08 23:25:02 +0100

    import xml and slightly changed representation of a net

--- src/clotri/core.clj
+++ src/clotri/core.clj
@@ -1,39 +1,51 @@
 (ns clotri.core
+  (:require clojure.xml) 
   (:gen-class))
 
-(defn- has-space? [place n {{c place} :capacity}] (if c (<= n c) true))
-(defn- tokens [place state] (let [t (state place)] (if t t 0)))
-(defn- lookup [transition direction {ts direction}]  (filter #(= transition (:name %)) ts))
-(defn- arrows [t net] [(lookup t :in net)  (lookup t :out net)])
+                                        
+(defn active? [{state :state arcs :arcs} t]
+  (let [a (filter (fn [{dst :dst}] (= dst t)) arcs)]
+    (every? (fn [{:keys [src weight]}]
+              (<= weight (state src)))
+            a)))
 
-(defn- weight [{w :weight}] (if w w 1))
-(defn- project [{in :in out :out} target] (let [a (concat in out)] (into #{} (map (fn [{p target}] p) a))))
-(defn places [x] (project x :place))
-(defn transition-names [x] (project x :name))
+(defn enabled [{ts :transitions :as net}] (filter (partial active? net) ts))
 
-(defn active? [t net]
-  (let [[in out] (arrows t net)]
-    (and (every? (fn [x]
-                   (let [w (weight x)
-                         p (:place x)]
-                     (<= w (tokens p (:state net))))) in)
-         (every? (fn [x]
-                   (let [w (weight x)
-                         p (:place x)]
-                     (has-space? p w net))) out))))
+(defn- nextstate [{state :state arcs :arcs :as net} t]
+  (let [in (filter (fn [{dst :dst}] (= dst t)) arcs)
+        out (filter (fn [{src :src}] (= src t)) arcs)
+        s (reduce (fn [sx {w :weight p :src}] (assoc sx p (- (sx p) w))) state in)
+        ns (reduce (fn [sx {w :weight p :dst}] (assoc sx p (+ (sx p) w))) s out)]
+    (assoc net :state ns)))
+
+(defn fire [t net] (if (active? net t) (nextstate net t) net))
+
+(def empty-net {:places [] :transitions [] :arcs [] :state {}})
+
+(defn extract-place [{:keys [places state] :as net}
+                     {{id :id} :attrs  c :content}]
+  (let [m (first (filter (fn [{tag :tag}] (= :initialMarking tag)) c))
+        t (first (:content m))
+        v (read-string (first (:content t)))]
+    (assoc net :places (conj places id) :state (assoc state id v))))
+
+(defn extract-arc [{arcs :arcs :as net} item]
+  (let [src (get-in item [:attrs :source])
+        dst (get-in item [:attrs :target])
+        m (first (filter (fn [{tag :tag}] (= :inscription tag)) (:content item)))
+        c (first (filter (fn [{tag :tag}] (= :text tag)) (:content m)))
+        n (read-string (first (:content c)))]
+    (assoc net :arcs (conj arcs {:src src :dst dst :weight n}))))
+
+(defn extract [net {tag :tag :as item}]
+  (cond (= tag :place) (extract-place net item)
+        (= tag :transition) (assoc net :transitions (conj (:transitions net) (get-in item [:attrs :id])))
+        (= tag :arc) (extract-arc net item)
+        :otherwise net))
+
+(defn read-xml [file] (let [fx (clojure.xml/parse file)] (reduce #(extract % %2) empty-net (-> fx :content first :content))))
+
+(def n (read-xml "nets/testnet.xml")) 
 
 
-(defn enabled [net] (filter #(active? % net) (transition-names net)))
-
-;; assumes that the transition is active
-(defn- nextstate [t net] (let [[in out] (arrows t net)
-                               s (:state net)
-                               si (reduce (fn [sx x] (let [w (weight x) p (:place x)] (assoc sx p (- (tokens p sx) w)))) s in)
-                               so (reduce (fn [sx x] (let [w (weight x) p (:place x)] (assoc sx p (+ (tokens p sx) w)))) si out)]
-                           so))
-
-(defn fire [t net] (if (active? t net) (assoc net :state (nextstate t net)) net))
-
-(defn add-transition [direction name place weight net] (let [c (net direction) curr (if c c {})] (assoc net direction (conj curr {:name name :place place :weight weight}))))
-(defn set-capacity [place capa net] (assoc-in net [:capacity place] capa))
 
